4664bb4bf4b9d949c973d17530d61ca82094aa34
christine parsons <clong9712@gmail.com>
2024-04-04 12:33:51 -0400

    SOLUTION: DFS max depth

--- DFS/solutions.js
+++ DFS/solutions.js
@@ -7,10 +7,20 @@
     A binary tree's maximum depth is the number of nodes along the longest
     path from the root node down to the farthest leaf node.
  **********************************/
-  export const maxDepth = (root) =>{
-    let depth = 0;
-    return 0;
-  }
+    var maxDepth = function(root) {
+      if (!root) return 0;
+      let stack = [[root, 1]];
+      let deepestDepth = 0;
+      while (stack.length){
+          let [curr, currDepth] = stack.pop();
+          if (curr){
+              deepestDepth = Math.max(currDepth, deepestDepth)
+              if (curr.left) stack.push([curr.left, currDepth + 1])
+              if(curr.right) stack.push([curr.right, currDepth + 1])
+          }
+      }
+      return deepestDepth
+  };
 
  /*******************************
   Path Sum
