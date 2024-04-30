4bb8478eefa9a7ca984cbe607de8b46eb2809efe
christine parsons <clong9712@gmail.com>
2024-04-29 21:01:15 -0400

    10 week day 1 progress added

--- 10-week-Rotation/arraysAndStrings.js
+++ 10-week-Rotation/arraysAndStrings.js
@@ -1,4 +1,21 @@
 /*******************************
  * Day 1: Iteration
- * https://leetcode.com/problems/move-zeroes/description/
  ********************************/
+
+// Problem 1: Move Zeroes
+// non working solution:
+var moveZeroes = function(nums) {
+    let p1;
+    let p2 = 0;
+    while (p2 < nums.length){
+        if(nums[p2] === 0){
+            p1 = p2;
+            while (nums[p2] === 0) p2++;
+            [nums[p1], nums[p2] ]= [nums[p2], nums[p1]];
+            p2 = p1+1
+        }
+        p2++;
+    }
+
+    return nums
+};
