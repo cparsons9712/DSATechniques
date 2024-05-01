4aea3f8a972c27efff00ab475f9dd152bc6b4e8c
christine parsons <clong9712@gmail.com>
2024-04-30 21:57:11 -0400

    working on merge

--- 10-week-Rotation/arraysAndStrings.js
+++ 10-week-Rotation/arraysAndStrings.js
@@ -1,21 +1,35 @@
 /*******************************
  * Day 1: Iteration
  ********************************/
+// two sum leetcode problem 1
+var twoSum = function(nums, target) {
 
-// Problem 1: Move Zeroes
-// non working solution:
-var moveZeroes = function(nums) {
-    let p1;
-    let p2 = 0;
-    while (p2 < nums.length){
-        if(nums[p2] === 0){
-            p1 = p2;
-            while (nums[p2] === 0) p2++;
-            [nums[p1], nums[p2] ]= [nums[p2], nums[p1]];
-            p2 = p1+1
+    let seen = {};
+    for (i in nums){
+        let num = nums[i];
+        let compliment = target - num;
+        if(seen[compliment]) return ([seen[compliment], i])
+        if(!seen[num]) seen[num] = i;
+    }
+};
+
+
+var merge = function(nums1, m, nums2, n) {
+    let a = nums1[m-1]
+    let b = nums2[n-1]
+    let p = nums1.length-1;
+    while (p >= 0){
+        if(a > b ){
+            nums1[p] = a;
+            m--;
+            a = nums1[m]
+        }else{
+            nums1[p] = b;
+            n--;
+            b = nums2[n]
         }
-        p2++;
+        p--;
     }
 
-    return nums
+
 };
